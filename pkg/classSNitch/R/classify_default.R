fc20dc606bb0a9ef42232a20012b397653b0f84a
ctolson (Chanin Tolson) <ctolson@email.unc.edu>
2015-09-04 02:19:52 +0000

    added classify_default.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/classsnitch@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/classSNitch/R/classify_default.R
@@ -0,0 +1,11 @@
+#' @title classify_default
+#' @name classify_default
+#' @aliases classify_default
+#' @docType data
+#' @export classify_default
+#'
+#' @keywords datasets
+#'
+#' @references \href{http://rmdb.stanford.edu/}{RNA Mapping Database}
+#'
+NULL

187c711b36fad608bb090de9c4a06c2359f34421
ctolson (Chanin Tolson) <ctolson@email.unc.edu>
2016-01-20 22:00:06 +0000

    documentation update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/classsnitch@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/classSNitch/R/normalize.R
+++ pkg/classSNitch/R/normalize.R
@@ -2,13 +2,14 @@
 #' @title normalize
 #' @aliases normalize
 #' @keywords normalize between-sample RNA
-#' @usage normalize(sample, base=sample[1,], margin=1, outbase=FALSE)
+#' @usage normalize(sample, base=sample[1,], margin=1, outbase=FALSE, mean=1.5)
 #' @param sample A numeric matrix containing values to be normalized (e.g. a set of mutant SHAPE traces).
 #' @param base An optional numeric vector containing the values to which the sample is to be normalized (e.g. a wild type SHAPE trace). Default is the first trace in sample.
 #' @param margin An optional number indicating if sample is organized by rows or columns, where 1 indicates rows and 2 indicates columns. Default is 1.
 #' @param outbase An optional boolean indicating if the normalized base should be returned. Default is FALSE.
+#' @param mean An optional number setting the mean SHAPE value. Default is 1.5.
 #' @export
-#' @details This function normalizes the average value of the base vector to 1.5. Each row (or column) in sample is then normalized by minimizing the absolute difference between the base and the sample row (or column). 
+#' @details This function normalizes the average value of the base vector. Each row (or column) in sample is then normalized by minimizing the absolute difference between the base and the sample row (or column). 
 #' @return 
 #' \describe{
 #'  \item{"samp_norm"}{A normalized numeric matrix with the same dimensions as sample.} 
@@ -27,7 +28,7 @@
 #' #normalize
 #' samp_norm = normalize(sample, base)
 #'
-normalize = function(sample, base=sample[1,], margin=1, outbase=FALSE){
+normalize = function(sample, base=sample[1,], margin=1, outbase=FALSE, mean=1.5){
   
   #set optional paramater margin
   if(missing(margin)) {
@@ -56,12 +57,19 @@ normalize = function(sample, base=sample[1,], margin=1, outbase=FALSE){
     outbase = outbase
   }
   
+  #set optional paramater mean
+  if(missing(mean)) {
+    mean = 1.5
+  } else {
+    mean = mean
+  }
+  
   #remove large negative values
   base[base<(-0.5)] = NA
   sample[sample<(-0.5)] = NA
   
-  #set average reactivity for wild-type to 1.5 
-  base = (1.5*sum(!is.na(base))/sum(base, na.rm=T))*base
+  #set average reactivity for wild-type 
+  base = (mean*sum(!is.na(base))/sum(base, na.rm=T))*base
   base[base<(-0.5)] = NA
   
   #function to optimize difference between wild-type and sample
